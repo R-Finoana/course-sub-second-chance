ab1ef3bbca76c86d5f3589bb32762b2fc28a16f7
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-12 14:03:04 +0300

    feat: add SubscriptionCreated using consumer and event for asynchronous email sending

---
+++ src/main/java/com/school/hei/endpoint/event/model/SubscriptionCreated.java
@@ -0,0 +1,25 @@
+package com.school.hei.endpoint.event.model;
+
+import com.school.hei.model.Subscription;
+import java.time.Duration;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class SubscriptionCreated extends PojaEvent {
+  private Subscription subscription;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(45);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(60);
+  }
+}
